4d105575ad15b3a29fb1950ce1989115bf2e7a0a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-07-05 08:31:15 +0800

    Take 1 Bench: 1691164

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -25,7 +25,8 @@ impl SearchHelpers {
     }
 
     pub fn get_explore_scaling(params: &MctsParams, parent: &Edge) -> f32 {
-        (params.expl_tau() * (parent.visits().max(1) as f32).ln()).exp()
+        let total_visits = parent.visits().max(1) as f32;
+        (params.expl_tau() * (total_visits.powf(0.95) as f32).ln()).exp()
     }
 
     pub fn get_fpu(parent: &Edge) -> f32 {

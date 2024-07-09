8e9acc3b42160aa7f44a885ce4f89f352d44d4f4
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-07-09 20:48:54 +0800

    Run rustfmt Bench: 2149456

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -60,14 +60,15 @@ impl SearchHelpers {
     /// This will be overriden by a `go movetime` command,
     /// and a move overhead will be applied to this, so no
     /// need for it here.
-    pub fn get_time(time: u64, increment: Option<u64>, ply: u16, movestogo: Option<u64>, params: MctsParams) -> u128 {
+    pub fn get_time(
+        time: u64,
+        increment: Option<u64>,
+        ply: u16,
+        movestogo: Option<u64>,
+        params: MctsParams,
+    ) -> u128 {
         let mut max_time;
-        let inc =
-            if let Some(i) = increment {
-                i as u64
-            } else {
-                0
-            };
+        let inc = if let Some(i) = increment { i as u64 } else { 0 };
 
         // Maximum move horizon of 30 moves
         let tm_mode; // true for increment mode, false for cyclic
@@ -83,11 +84,20 @@ impl SearchHelpers {
 
         if tm_mode {
             let log_time = (time_left / 1000.0).log10();
-            let opt_constant = (params.tm_opt_value1() / 100.0 + params.tm_opt_value2() / 1000.0 * log_time).min(params.tm_opt_value3() / 100.0);
-            let opt_scale = (params.tm_scale_value1() / 100.0 + (ply as f64 + params.tm_scale_value2()).powf(params.tm_scale_value3()) * opt_constant).min(params.tm_scale_value4() * time as f64 / time_left);
+            let opt_constant = (params.tm_opt_value1() / 100.0
+                + params.tm_opt_value2() / 1000.0 * log_time)
+                .min(params.tm_opt_value3() / 100.0);
+            let opt_scale = (params.tm_scale_value1() / 100.0
+                + (ply as f64 + params.tm_scale_value2()).powf(params.tm_scale_value3())
+                    * opt_constant)
+                .min(params.tm_scale_value4() * time as f64 / time_left);
             // More time at the start of the game
             let bonus_ply = params.tm_bonus_ply();
-            let bonus = if ply <= bonus_ply as u16 - 1 { 1.0 + (bonus_ply - ply as f64).log10() * params.tm_bonus_value1() } else { 1.0 };
+            let bonus = if ply <= bonus_ply as u16 - 1 {
+                1.0 + (bonus_ply - ply as f64).log10() * params.tm_bonus_value1()
+            } else {
+                1.0
+            };
             max_time = (opt_scale * bonus * time_left) as u128;
         } else {
             max_time = (time / mtg) as u128;

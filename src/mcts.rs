63524bd5faa5889179c7a6f483500b19819f307f
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-07-24 20:10:53 +0800

    Take 1 Bench: 2796786

--- src/mcts.rs
+++ src/mcts.rs
@@ -266,8 +266,9 @@ impl<'a> Searcher<'a> {
         let cpuct = SearchHelpers::get_cpuct(&self.params, edge, is_root);
         let fpu = SearchHelpers::get_fpu(edge);
         let expl_scale = SearchHelpers::get_explore_scaling(&self.params, edge);
+        let gini_impurity = 0.50 + node.gini_impurity().ln_1p();
 
-        let expl = cpuct * expl_scale;
+        let expl = cpuct * expl_scale * gini_impurity;
 
         self.tree.get_best_child_by_key(ptr, |action| {
             let q = SearchHelpers::get_action_value(action, fpu);

b3f1d8a8772de9bb32d0788f0a2a43ea014b482c
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2024-07-24 09:14:20 +0800

    Take 2, opposite lol, idk Bench: 2433900

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -55,10 +55,10 @@ impl SearchHelpers {
     }
 
     /// Get the optimism scaling based on the q value of the root node
-    /// A lower q value encourages more exploration.
+    /// A higher q value encourages more exploration.
     pub fn get_optimism_scaling(params: &MctsParams, q: f32) -> f32 {
         let scale =
-            1.0 + params.optimism1() * (params.optimism2() - q) / (q.abs() + params.optimism3());
+            1.0 + params.optimism1() * (params.optimism2() + q) / (q.abs() + params.optimism3());
         scale.max(0.3)
     }
 
